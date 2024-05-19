aefff57cc72d41cecae2c73a605ec921fe1bc92f
Denis Rusanov <eneanuch@yandex.ru>
2024-05-19 19:15:01 +0300

    added logging for web app

--- web/server.py
+++ web/server.py
@@ -1,4 +1,8 @@
+import json
+import logging
 import os
+import sys
+from logging.config import dictConfig
 
 from fastapi import FastAPI, HTTPException
 from starlette.responses import FileResponse, JSONResponse
@@ -7,6 +11,12 @@ from models import SendPayload
 
 app = FastAPI()
 root = os.path.dirname(os.path.abspath(__file__))
+
+with open('logging_config.json', 'r') as f:
+    logging_config = json.load(f)
+    dictConfig(logging_config)
+
+logger = logging.getLogger(__name__)
 
 # run with uvicorn web:app --host 0.0.0.0 --port 80
 
@@ -34,18 +44,18 @@ async def send_results(payload: SendPayload):
         case "sms":
             if payload.phoneNumber is None:
                 raise HTTPException(status_code=400, detail="Нужен телефон для отправки сообщения")
-            print(f"Отправка SMS по номеру {payload.phoneNumber}. Данные: {payload.data}")
+            logger.info(f"Отправка SMS по номеру {payload.phoneNumber}. Данные: {payload.data}")
 
         case "i2c":
             if payload.i2cAddress is None or payload.i2cBus is None:
                 raise HTTPException(status_code=400, detail="I2C адрес и шина нужны для отправки данных")
-            print(f"Отправка данных через I2C по адресу {payload.i2cAddress} на шину {payload.i2cBus}. Данные: {payload.data}")
+            logger.info(f"Отправка данных через I2C по адресу {payload.i2cAddress} на шину {payload.i2cBus}. Данные: {payload.data}")
 
         case "uart":
             if payload.uartPort is None or payload.baudRate is None or payload.timeout is None:
                 raise HTTPException(status_code=400,
                                     detail="UART порт, скорость, таймаут нужны для отправки данных по UART")
-            print(
+            logger.info(
                 f"Отправка данных по UART по порту {payload.uartPort} со скоростью {payload.baudRate} и таймаутом {payload.timeout}. Данные: {payload.data}")
 
         case _:
