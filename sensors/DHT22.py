d2ac9a07159183b7909e8690183fc2107180da47
Denis Rusanov <eneanuch@yandex.ru>
2024-05-27 16:13:11 +0300

    new dht library

--- sensors/DHT22.py
+++ sensors/DHT22.py
@@ -1,18 +1,20 @@
-import Adafruit_DHT as dht
+import board
+
+import adafruit_dht
 
 from sensors.sensor_head import SensorHead
 
 
 class DHT22(SensorHead):
 
-    def __init__(self, dht_pin1=18, dht_pin2=16):
+    def __init__(self, dht_pin1=board.D18, dht_pin2=board.D16):
         super().__init__()
         self.system_name = 'dht'
-        self.pins = [dht_pin1, dht_pin2]
+        self.devices = [adafruit_dht.DHT22(dht_pin1), adafruit_dht.DHT22(dht_pin2)]
 
     def get_data(self, channel: int = 22) -> dict[str: str]:
         # Считает среднее с трех датчиков
-        data = [dht.read_retry(dht.DHT22, i) for i in self.pins]
+        data = [(device.temperature, device.humidity) for device in self.devices]
 
         temperature = round(sum(map(lambda x: x[1], data)) / 3, 2)
         humidity = round(sum(map(lambda x: x[0], data)) / 3, 2)
