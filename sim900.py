e80994277ed4953afc3a24fa4fb52f70a5fb2d86
Denis Rusanov <eneanuch@yandex.ru>
2024-05-27 17:15:09 +0300

    sim900

--- sim900.py
+++ sim900.py
@@ -11,23 +11,23 @@ port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
 # Transmitting AT Commands to the Modem
 # '\r\n' indicates the Enter key
 
-port.write('AT' + '\r\n')
+port.write(('AT' + '\r\n').encode())
 rcv = port.read(10)
 print(rcv)
 time.sleep(1)
 
-port.write('ATE0' + '\r\n')  # Disable the Echo
+port.write(('ATE0' + '\r\n').encode())  # Disable the Echo
 rcv = port.read(10)
 print(rcv)
 time.sleep(1)
 
-port.write('AT+CMGF=1' + '\r\n')  # Select Message format as Text mode
+port.write(('AT+CMGF=1' + '\r\n').encode())  # Select Message format as Text mode
 rcv = port.read(10)
 print(rcv)
 
 time.sleep(1)
 
-port.write('AT+CNMI=2,1,0,0,0' + '\r\n')  # New SMS Message Indications
+port.write(('AT+CNMI=2,1,0,0,0' + '\r\n').encode())  # New SMS Message Indications
 rcv = port.read(10)
 print(rcv)
 
@@ -35,18 +35,18 @@ time.sleep(1)
 
 # Sending a message to a particular Number
 
-port.write('AT+CMGS="XXXXXXXXXX"' + '\r\n')
+port.write(('AT+CMGS="XXXXXXXXXX"' + '\r\n').encode())
 rcv = port.read(10)
 print(rcv)
 
 time.sleep(1)
 
-port.write('Hello User' + '\r\n')  # Message
+port.write(('Hello User' + '\r\n').encode())  # Message
 rcv = port.read(10)
 print(rcv)
 
 
-port.write("\x1A")  # Enable to send SMS
+port.write(("\x1A").encode())  # Enable to send SMS
 for i in range(10):
     rcv = port.read(10)
     print(rcv)
