26b9091aa05a9e5cd5bdb371c00393b92f067e2b
Denis Rusanov <eneanuch@yandex.ru>
2024-05-31 12:51:29 +0300

    fix 32 bytes limitations

--- senders/i2c_sender.py
+++ senders/i2c_sender.py
@@ -17,5 +17,12 @@ class I2CSender(SenderHead):
     def send_data(self, data: I2CData) -> SenderData:
         bus = smbus2.SMBus(data.i2cBus)
         byte_value = self.string_to_bytes(data.data)
-        bus.write_i2c_block_data(data.i2cAddress, 0x00, byte_value)
-        return SenderData(status=Statuses.SUCCESS, message=data.data)
+
+        chunk_size = 32
+        try:
+            for i in range(0, len(byte_value), chunk_size):
+                chunk = byte_value[i:i + chunk_size]
+                bus.write_i2c_block_data(data.i2cAddress, 0x00, chunk)
+            return SenderData(status=Statuses.SUCCESS, message=data.data)
+        except ValueError as e:
+            return SenderData(status=Statuses.FAILURE, message=str(e))
