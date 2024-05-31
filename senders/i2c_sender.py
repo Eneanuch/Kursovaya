1c96a9e3e353f761dd3a135fa4c8a446b5a6a559
Denis Rusanov <eneanuch@yandex.ru>
2024-05-31 12:40:19 +0300

    new approach to sending data

--- senders/i2c_sender.py
+++ senders/i2c_sender.py
@@ -7,8 +7,15 @@ class I2CSender(SenderHead):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
+    @staticmethod
+    def string_to_bytes(data: str):
+        byte_value = []
+        for c in data:
+            byte_value.append(ord(c))
+        return byte_value
+
     def send_data(self, data: I2CData) -> SenderData:
         bus = smbus.SMBus(data.i2cBus)
-        byte_data = data.data.encode('utf-8')
-        bus.write_i2c_block_data(data.i2cAddress, 0, list(byte_data))
-        return SenderData(status=Statuses.SUCCESS, message=byte_data)
+        byte_value = self.string_to_bytes(data.data)
+        bus.write_i2c_block_data(data.i2cAddress, 0x00, byte_value)
+        return SenderData(status=Statuses.SUCCESS, message=data.data)
