afe51fb19bf9e285a253b17194f669d14434f118
Denis Rusanov <eneanuch@yandex.ru>
2024-06-04 14:09:54 +0300

    add test uart file

--- sim900.py
+++ sim900.py
@@ -18,35 +18,31 @@ time.sleep(1)
 
 port.write(('ATE0' + '\r\n').encode())  # Disable the Echo
 rcv = port.read(10)
-print(rcv)
+print(rcv.decode('utf-8'))
 time.sleep(1)
 
 port.write(('AT+CMGF=1' + '\r\n').encode())  # Select Message format as Text mode
 rcv = port.read(10)
-print(rcv)
-
+print(rcv.decode('utf-8'))
 time.sleep(1)
 
 port.write(('AT+CNMI=2,1,0,0,0' + '\r\n').encode())  # New SMS Message Indications
 rcv = port.read(10)
-print(rcv)
-
+print(rcv.decode('utf-8'))
 time.sleep(1)
 
 # Sending a message to a particular Number
 
 port.write(('AT+CMGS="XXXXXXXXXX"' + '\r\n').encode())
 rcv = port.read(10)
-print(rcv)
-
+print(rcv.decode('utf-8'))
 time.sleep(1)
 
 port.write(('Hello User' + '\r\n').encode())  # Message
 rcv = port.read(10)
-print(rcv)
-
+print(rcv.decode('utf-8'))
 
 port.write(("\x1A").encode())  # Enable to send SMS
 for i in range(10):
     rcv = port.read(10)
-    print(rcv)
+    print(rcv.decode('utf-8'))
