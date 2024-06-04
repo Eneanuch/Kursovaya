710dff5c9a9f00f93a7534beb4467e6aa79df1c3
Denis Rusanov <eneanuch@yandex.ru>
2024-06-04 14:21:22 +0300

    new tests

--- sim900.py
+++ sim900.py
@@ -10,14 +10,27 @@ ser = serial.Serial("/dev/serial0", baudrate=9600, timeout=1)
 
 def send_command(command, wait_for_response=True):
     ser.write((command + '\r').encode())
-    time.sleep(1)  # Небольшая задержка для обработки команды
+    time.sleep(1)
 
     if wait_for_response:
         response = ser.read_all().decode()
         return response
     return None
 
-# Пример использования
+def send_sms(phone_number, message):
+    time.sleep(1)
+
+    send_command('AT+CMGF=1')
+
+    send_command('AT+CMGS="{}"'.format(phone_number))
+    send_command(message, wait_for_response=False)
+    ser.write(bytes([26]))
+    time.sleep(1)
+
+    response = ser.read_all().decode()
+    print(response)
+
+
 def main():
     response = send_command('AT')
     print('Response:', response)
@@ -25,13 +38,15 @@ def main():
     response = send_command('AT+CSQ')
     print('Signal Quality:', response)
 
-    while True:
-        line = ser.readline().decode().strip()
-        if line:
-            print('Received:', line)
-            if 'BU,DNHG>2' in line:
-                print('Special message received:', line)
-                # Выполнить нужные действия при получении этого сообщения
+    send_sms("+79807998558", "Hello Worlds")
+    time.sleep(10)
+
+    # while True:
+    #     line = ser.readline().decode().strip()
+    #     if line:
+    #         print('Received:', line)
+    #         time.sleep(1)
+
 
 if __name__ == '__main__':
     main()
